ea43ab98f3037d93e6d864cadfc107d0a97a317c
AmitKatz <amitkatz97@gmail.com>
2024-12-17 21:01:45 +0200

    config fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { TRUE } from 'sass'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build:{
+    outDir:'../faddomBackend/public',
+    emptyOutDir: TRUE
+  }
 })
